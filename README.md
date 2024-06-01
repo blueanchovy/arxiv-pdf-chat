c584370f8ad05e9da4024549c5057653dd7bd861
Manish Jha <manishjhazyx@gmail.com>
2024-06-01 19:37:07 +0530

    feat: notes generation through openai by specifying result doc structure and prompt for notes generation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Development
 
+Before starting the api server run `docker run -p 8000:8000 -d --rm --name unstructured-api quay.io/unstructured-io/unstructured-api:latest --port 8000 --host 0.0.0.0`
+
 This monorepo uses Turborepo to manage the two packages (web, api).
 To build all packages run `yarn build` from root.
 
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { PDFDocument } from "pdf-lib";
 import { Document } from "langchain/document";
-import { writeFile, unlink } from "fs/promises";
+import { writeFile, unlink, readFile } from "fs/promises";
 import { UnstructuredLoader } from "langchain/document_loaders/fs/unstructured";
+import { formatDocumentsAsString } from "langchain/util/document";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import {
+  ArxivPaperNote,
+  NOTES_TOOL_SCHEMA,
+  NOTE_PROMPT,
+  outputParser,
+} from "prompts.js";
 
 async function deletePages(
   pdf: Buffer,
@@ -44,6 +52,25 @@ async function convertPdfToDocuments(pdf: Buffer): Promise<Array<Document>> {
   return documents;
 }
 
+async function generateNotes(
+  documents: Array<Document>
+): Promise<Array<ArxivPaperNote>> {
+  const documentsAsString = formatDocumentsAsString(documents);
+  const model = new ChatOpenAI({
+    modelName: "gpt-4-1106-preview",
+    temperature: 0.0,
+  });
+
+  const modelWithTool = model.bind({ tools: [NOTES_TOOL_SCHEMA] });
+
+  const chain = NOTE_PROMPT.pipe(modelWithTool).pipe(outputParser);
+  const response = await chain.invoke({
+    paper: documentsAsString,
+  });
+  return response;
+}
+
+//to fetch and convert pdf to llm friendly structured doc
 async function main({
   paperUrl,
   name,
@@ -64,8 +91,13 @@ async function main({
   }
 
   const documents = await convertPdfToDocuments(pdfAsBuffer);
-  console.log(documents);
-  console.log("length", documents.length);
+  // const notes = await generateNotes(documents);
+
+  // const docs = await readFile(`pdfs/dwy88.pdf`, "utf-8");
+  // const parsedDocs: Array<Document> = JSON.parse(documents);
+  const notes = await generateNotes(documents);
+  console.log(notes);
+  console.log("length", notes.length);
 }
 
-main({ paperUrl: "https://arxiv.org/pdf/2405.13945.pdf", name: "test" });
+main({ paperUrl: "https://arxiv.org/pdf/2305.15334.pdf", name: "test" });
